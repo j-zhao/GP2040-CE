3aa5360c2e294ce30ef30a125f8b157c8c69c274
Jesse Zhao <jesse.zhao@epicgames.com>
2025-07-15 21:26:14 -0700

    Remove comments

--- configs/MustfunBoard/BoardConfig.h
+++ configs/MustfunBoard/BoardConfig.h
@@ -12,27 +12,27 @@
 #define BOARD_CONFIG_LABEL "MustfunBoard"
 
 // Main pin mapping Configuration
-//                                                   // GP2040 | Xinput | Switch  | PS3/4/5  | Dinput | Arcade |
-#define GPIO_PIN_02 GpioAction::BUTTON_PRESS_A2    // A2     |        |         | Touchpad |        |        |
-#define GPIO_PIN_03 GpioAction::BUTTON_PRESS_B3    // B3     | X      | Y       | Square   | 1      | P1     |
-#define GPIO_PIN_04 GpioAction::BUTTON_PRESS_B4    // B4     | Y      | X       | Triangle | 4      | P2     |
-#define GPIO_PIN_05 GpioAction::BUTTON_PRESS_R1    // R1     | RB     | R       | R1       | 6      | P3     |
-#define GPIO_PIN_06 GpioAction::BUTTON_PRESS_L1    // L1     | LB     | L       | L1       | 5      | P4     |
-#define GPIO_PIN_07 GpioAction::BUTTON_PRESS_B1    // B1     | A      | B       | Cross    | 2      | K1     |
-#define GPIO_PIN_08 GpioAction::BUTTON_PRESS_B2    // B2     | B      | A       | Circle   | 3      | K2     |
-#define GPIO_PIN_09 GpioAction::BUTTON_PRESS_R2    // R2     | RT     | ZR      | R2       | 8      | K3     |
-#define GPIO_PIN_10 GpioAction::BUTTON_PRESS_L2    // L2     | LT     | ZL      | L2       | 7      | K4     |
-#define GPIO_PIN_11 GpioAction::BUTTON_PRESS_R3    // R3     | RS     | RS      | R3       | 12     | RS     |
-#define GPIO_PIN_12 GpioAction::BUTTON_PRESS_UP    // UP     | UP     | UP      | UP       | UP     | UP     |
-#define GPIO_PIN_13 GpioAction::BUTTON_PRESS_L3    // L3     | LS     | LS      | L3       | 11     | LS     |
-#define GPIO_PIN_14 GpioAction::BUTTON_PRESS_RIGHT // RIGHT  | RIGHT  | RIGHT   | RIGHT    | RIGHT  | RIGHT  |
-#define GPIO_PIN_15 GpioAction::BUTTON_PRESS_DOWN  // DOWN   | DOWN   | DOWN    | DOWN     | DOWN   | DOWN   |
-#define GPIO_PIN_16 GpioAction::BUTTON_PRESS_LEFT  // LEFT   | LEFT   | LEFT    | LEFT     | LEFT   | LEFT   |
-// #define GPIO_PIN_17 GpioAction::BUTTON_PRESS_MACRO_1 // M1     | M1     | M1      | M1       | M1     | M1     |
-#define GPIO_PIN_20 GpioAction::BUTTON_PRESS_A2 // A2     | ~      | Capture | ~        | 14     | ~      |
-#define GPIO_PIN_21 GpioAction::BUTTON_PRESS_S1 // S1     | Back   | Minus   | Share    | 9      | Coin   |
-#define GPIO_PIN_23 GpioAction::BUTTON_PRESS_S2 // S2     | Start  | Plus    | Options  | 10     | Start  |
-#define GPIO_PIN_24 GpioAction::BUTTON_PRESS_A1 // A1     | Guide  | Home    | PS       | 13     | ~      |
+//                                                 // GP2040 | Xinput | Switch  | PS3/4/5  | Dinput | Arcade |
+#define GPIO_PIN_02 GpioAction::BUTTON_PRESS_A2      // A2     |        |         | Touchpad |        |        |
+#define GPIO_PIN_03 GpioAction::BUTTON_PRESS_B3      // B3     | X      | Y       | Square   | 1      | P1     |
+#define GPIO_PIN_04 GpioAction::BUTTON_PRESS_B4      // B4     | Y      | X       | Triangle | 4      | P2     |
+#define GPIO_PIN_05 GpioAction::BUTTON_PRESS_R1      // R1     | RB     | R       | R1       | 6      | P3     |
+#define GPIO_PIN_06 GpioAction::BUTTON_PRESS_L1      // L1     | LB     | L       | L1       | 5      | P4     |
+#define GPIO_PIN_07 GpioAction::BUTTON_PRESS_B1      // B1     | A      | B       | Cross    | 2      | K1     |
+#define GPIO_PIN_08 GpioAction::BUTTON_PRESS_B2      // B2     | B      | A       | Circle   | 3      | K2     |
+#define GPIO_PIN_09 GpioAction::BUTTON_PRESS_R2      // R2     | RT     | ZR      | R2       | 8      | K3     |
+#define GPIO_PIN_10 GpioAction::BUTTON_PRESS_L2      // L2     | LT     | ZL      | L2       | 7      | K4     |
+#define GPIO_PIN_11 GpioAction::BUTTON_PRESS_R3      // R3     | RS     | RS      | R3       | 12     | RS     |
+#define GPIO_PIN_12 GpioAction::BUTTON_PRESS_UP      // UP     | UP     | UP      | UP       | UP     | UP     |
+#define GPIO_PIN_13 GpioAction::BUTTON_PRESS_L3      // L3     | LS     | LS      | L3       | 11     | LS     |
+#define GPIO_PIN_14 GpioAction::BUTTON_PRESS_RIGHT   // RIGHT  | RIGHT  | RIGHT   | RIGHT    | RIGHT  | RIGHT  |
+#define GPIO_PIN_15 GpioAction::BUTTON_PRESS_DOWN    // DOWN   | DOWN   | DOWN    | DOWN     | DOWN   | DOWN   |
+#define GPIO_PIN_16 GpioAction::BUTTON_PRESS_LEFT    // LEFT   | LEFT   | LEFT    | LEFT     | LEFT   | LEFT   |
+#define GPIO_PIN_17 GpioAction::BUTTON_PRESS_MACRO_1 // M1     | M1     | M1      | M1       | M1     | M1     |
+#define GPIO_PIN_20 GpioAction::BUTTON_PRESS_A2      // A2     | ~      | Capture | ~        | 14     | ~      |
+#define GPIO_PIN_21 GpioAction::BUTTON_PRESS_S1      // S1     | Back   | Minus   | Share    | 9      | Coin   |
+#define GPIO_PIN_23 GpioAction::BUTTON_PRESS_S2      // S2     | Start  | Plus    | Options  | 10     | Start  |
+#define GPIO_PIN_24 GpioAction::BUTTON_PRESS_A1      // A1     | Guide  | Home    | PS       | 13     | ~      |
 
 // Setting GPIO pins to assigned by add-on
 //
@@ -92,39 +92,6 @@
 
 // #define BUTTON_LAYOUT BUTTON_LAYOUT_BOARD_DEFINED_A
 // #define BUTTON_LAYOUT_RIGHT BUTTON_LAYOUT_BOARD_DEFINED_B
-
-// B16
-// #define DEFAULT_BOARD_LAYOUT_A                                                     \
-//      {                                                                             \
-//           {GP_ELEMENT_PIN_BUTTON, {47, 19, 4, 4, 1, 1, 17, GP_SHAPE_SQUARE}},      \
-//               {GP_ELEMENT_PIN_BUTTON, {32, 27, 4, 4, 1, 1, 16, GP_SHAPE_SQUARE}},  \
-//               {GP_ELEMENT_PIN_BUTTON, {42, 27, 4, 4, 1, 1, 15, GP_SHAPE_SQUARE}},  \
-//               {GP_ELEMENT_PIN_BUTTON, {50, 32, 4, 4, 1, 1, 14, GP_SHAPE_SQUARE}},  \
-//               {GP_ELEMENT_PIN_BUTTON, {40, 45, 4, 4, 1, 1, 13, GP_SHAPE_SQUARE}},  \
-//               {GP_ELEMENT_PIN_BUTTON, {64, 17, 4, 4, 1, 1, 2, GP_SHAPE_SQUARE}},   \
-//               {GP_ELEMENT_PIN_BUTTON, {53, 49, 5, 5, 1, 1, 12, GP_SHAPE_SQUARE}},  \
-//               {GP_ELEMENT_PIN_BUTTON, {66, 45, 4, 4, 1, 1, 11, GP_SHAPE_SQUARE}},  \
-//               {GP_ELEMENT_PIN_BUTTON, {77, 15, 2, 2, 1, 1, 20, GP_SHAPE_ELLIPSE}}, \
-//               {GP_ELEMENT_PIN_BUTTON, {84, 15, 2, 2, 1, 1, 24, GP_SHAPE_ELLIPSE}}, \
-//               {GP_ELEMENT_PIN_BUTTON, {91, 15, 2, 2, 1, 1, 21, GP_SHAPE_ELLIPSE}}, \
-//           {                                                                        \
-//                GP_ELEMENT_PIN_BUTTON, { 98, 15, 2, 2, 1, 1, 23, GP_SHAPE_ELLIPSE } \
-//           }                                                                        \
-//      }
-
-// #define DEFAULT_BOARD_LAYOUT_B                                                    \
-//      {                                                                            \
-//           {GP_ELEMENT_PIN_BUTTON, {59, 27, 4, 4, 1, 1, 3, GP_SHAPE_SQUARE}},      \
-//               {GP_ELEMENT_PIN_BUTTON, {69, 25, 4, 4, 1, 1, 4, GP_SHAPE_SQUARE}},  \
-//               {GP_ELEMENT_PIN_BUTTON, {79, 25, 4, 4, 1, 1, 5, GP_SHAPE_SQUARE}},  \
-//               {GP_ELEMENT_PIN_BUTTON, {89, 27, 4, 4, 1, 1, 6, GP_SHAPE_SQUARE}},  \
-//               {GP_ELEMENT_PIN_BUTTON, {59, 37, 4, 4, 1, 1, 7, GP_SHAPE_SQUARE}},  \
-//               {GP_ELEMENT_PIN_BUTTON, {69, 35, 4, 4, 1, 1, 8, GP_SHAPE_SQUARE}},  \
-//               {GP_ELEMENT_PIN_BUTTON, {79, 35, 4, 4, 1, 1, 9, GP_SHAPE_SQUARE}},  \
-//           {                                                                       \
-//                GP_ELEMENT_PIN_BUTTON, { 89, 37, 4, 4, 1, 1, 10, GP_SHAPE_SQUARE } \
-//           }                                                                       \
-//      }
 
 // Keyboard Host enabled by default
 #define KEYBOARD_HOST_ENABLED 1
